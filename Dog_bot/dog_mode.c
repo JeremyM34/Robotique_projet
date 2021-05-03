112584767b45c3b6ad8b532324b0c5c7b9efd439
jeremy <jeremy.marciacq@epfl.ch>
2021-05-03 11:40:49 +0200

    Show_direction_led

--- Dog_bot/dog_mode.c
+++ Dog_bot/dog_mode.c
@@ -9,6 +9,7 @@
 //#include <sound_direction.h>
 #include <mapper.h>
 #include <motor_controller.h>
+#include <leds.h>
 
 static double direction_error = -135;
 
@@ -23,8 +24,10 @@ void dog_mode_setUp(void)
 void playTheDog(void)
 {	
 	follow_trajectory();
+	led_showDirection();
+
 	/*
-	if(directionAge>DIRECTION_TIMEOUT)
+	if(directionAge<DIRECTION_TIMEOUT)
 	{
 		led_showDirection();
 		led_showMood();
@@ -53,7 +56,42 @@ void follow_trajectory(void)
 ///////////// LEDS ////////////////
 void led_showDirection(void)
 {
+	float actual_error = get_actual_error(); //[deg] get precise error form motor_controller.c
+	
+	int led_number = round((actual_error+180)*8/360) + 1;
 
+	clear_leds();
+
+	switch (led_number)
+	{
+	case 1:
+		set_led(LED1, 1);
+		break;
+	case 2:
+		set_rgb_led(LED2, 255, 0, 0)
+		break;
+	case 3:
+		set_led(LED3, 1);
+		break;
+	case 4:
+		set_rgb_led(LED4, 255, 0, 0)
+		break;
+	case 5:
+		set_led(LED5, 1);
+		break;
+	case 6:
+		set_rgb_led(LED6, 255, 0, 0)
+		break;
+	case 7:
+		set_led(LED7, 1);
+		break;
+	case 8:
+		set_rgb_led(LED8, 255, 0, 0)
+		break;
+	case 9:
+		set_led(LED1, 1);
+		break;
+	}
 }
 
 void led_showMood(void)
@@ -63,6 +101,6 @@ void led_showMood(void)
 
 void led_standBy(void)
 {
-
+	static bool 
 }
 
