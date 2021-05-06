6d7b749d84c6dddf5b1b160b96909987f8ed2f80
jeremy <jeremy.marciacq@epfl.ch>
2021-05-06 11:59:51 +0200

    upset mode and states working

--- Dog_bot/dog_mode.c
+++ Dog_bot/dog_mode.c
@@ -12,22 +12,35 @@
 #include <leds.h>
 #include <spi_comm.h>
 
-#define STANDARD_DIRECTION_TIMEOUT	2 //[s]
-#define AVOIDANCE_TIMEOUT	2 //[s]
-#define TIME_BETWEEN_OBSTACLE 1 //[s]
+#define AVOIDANCE_TIMEOUT		1.5	//[s]
+#define TIME_BETWEEN_OBSTACLE	0.5	//[s]
+#define THRESHOLD_FOLLOW_ANGLE	50	//[deg]
+#define WALL_FOLLOW_DISTANCE	4	//[cm]
 
-enum STATE{STAND_BY, FOLLOWING, AVOIDING, UPSET};
+#define BODY_LED_PWM_FREQ		10000	//[Hz]
+#define BODY_LED_PWM_PERIOD		50	//[in ticks]
+#define BODY_LED_PULSE_SPEED_FACTOR	600. //[/ms] body_led glows from off to on in 1000/speed_factor seconds
+#define DUTY_CYCLE_MAX			10000
+#define MINIMUM_DUTY_CYCLE		200 //[0 - DUTY_CYCLE_MAX]
+
+#define UPSET_LED_TOGGLE_TIME	400 //[ms]
 
 #define STOMS(n)	n*1000
 
+
+enum STATES state = FOLLOWING;
+
+static bool state_change = TRUE;
+
 static float direction_error = -90;
-static int new_direction_flag = 1;
+static bool new_direction_flag = 1;
 
 static systime_t last_direction_time; //[ms]
-static int avoidance_manoeuver_flag = 0;
 static int lateral_distance = 0; //[cm]
 
-
+///////////////////////////
+////// BODY LED PWM ///////
+///////////////////////////
 static void body_led_callback_on(PWMDriver *pwmp)
 {
 	(void)pwmp;
@@ -41,17 +54,19 @@ static void body_led_callback_off(PWMDriver *pwmp)
 }
 
 static PWMConfig pwm_body_led_cfg = {
-	.frequency = 10000,
-	.period = 50,
+	.frequency = BODY_LED_PWM_FREQ,
+	.period = BODY_LED_PWM_PERIOD,
 	.cr2 = 0,
 	.callback = body_led_callback_on,
 	{
-		{.mode = PWM_OUTPUT_ACTIVE_HIGH, body_led_callback_off},          /* CH1 mode and callback.         */
-		{PWM_OUTPUT_DISABLED, NULL},             /* CH2 mode and callback.         */
-		{PWM_OUTPUT_DISABLED, NULL},             /* CH3 mode and callback.         */
-		{PWM_OUTPUT_DISABLED, NULL}              /* CH4 mode and callback.         */
+		{PWM_OUTPUT_DISABLED, body_led_callback_off},
+		{PWM_OUTPUT_DISABLED, NULL},
+		{PWM_OUTPUT_DISABLED, NULL},
+		{PWM_OUTPUT_DISABLED, NULL}
 	}
 };
+///////////////////////////
+///////////////////////////
 
 void dog_mode_setUp(void)
 {
@@ -61,19 +76,66 @@ void dog_mode_setUp(void)
 	spi_comm_start(); //For the RGB leds
 
 	pwmStart(&PWMD5, &pwm_body_led_cfg);
-
-	pwmEnableChannel(&PWMD5, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD5, 500));
-
-    pwmEnablePeriodicNotification(&PWMD5); // PWM general interrupt at the beginning of the period to handle motor steps.
-
-	pwmEnableChannelNotification(&PWMD5, 0);
+	pwmEnableChannel(&PWMD5, 0, 0);
 
 	last_direction_time = ST2MS(chVTGetSystemTime());
 }
 
 void playTheDog(void)
 {	
-	compute_trajectory();
+	if(state == STAND_BY)
+	{
+		if(state_change)
+		{
+			clear_leds();
+			set_front_led(0);
+			pwmEnablePeriodicNotification(&PWMD5); // Activate PWM for the body LED
+			pwmEnableChannelNotification(&PWMD5, 0);
+			stop();
+
+			state_change = FALSE;
+		}
+
+		led_standBy();
+	}
+	else if(state == UPSET)
+	{
+		if(state_change)
+		{
+			set_front_led(0);
+			stop();
+			led_showUpset(1);
+
+			state_change = FALSE;
+		}
+
+		led_showUpset(0);
+	}
+	else if(state == FOLLOWING || state == AVOIDING)
+	{
+		if(state_change)
+		{
+			pwmDisablePeriodicNotification(&PWMD5); // Activate PWM for the body LED
+			pwmDisableChannelNotification(&PWMD5, 0);
+			state_change = FALSE;
+		}
+
+		systime_t direction_timeout = STOMS(FOLLOWING_TIMEOUT);
+		systime_t directionAge = ST2MS(chVTGetSystemTime()) - last_direction_time;
+
+		if(state == AVOIDING)
+			direction_timeout = STOMS(AVOIDANCE_TIMEOUT);
+
+		if(directionAge > direction_timeout)
+		{
+			state = STAND_BY;
+			state_change = TRUE;
+		}
+
+		compute_trajectory();
+		follow_trajectory();
+		led_showDirection();
+	}
 
 	/*
 	if(get_sound_angle(&sound_direction))
@@ -81,25 +143,6 @@ void playTheDog(void)
 
 	}
 	*/
-
-	int direction_timeout;
-	int directionAge = ST2MS(chVTGetSystemTime()) - last_direction_time;
-
-	if(avoidance_manoeuver_flag)
-		direction_timeout = STOMS(AVOIDANCE_TIMEOUT);
-	else
-		direction_timeout = STOMS(STANDARD_DIRECTION_TIMEOUT);
-
-	if(directionAge<direction_timeout)
-	{
-		follow_trajectory();
-		led_showDirection();
-	}
-	else
-	{
-		stop();
-		led_standBy();
-	}
 }
 
 ///////////// MOVE ////////////////
@@ -107,7 +150,6 @@ void playTheDog(void)
 void compute_trajectory(void)
 {
 	static systime_t last_obstacle_time = 0;
-
 	static map_data map_info;
 
 	compute_map();
@@ -119,27 +161,32 @@ void compute_trajectory(void)
 		map_info.obstacle_flag = 0;
 		last_obstacle_time = ST2MS(chVTGetSystemTime());
 
-		if(fabsf(map_info.obstacle_direction) > 50)
+		if(fabsf(map_info.obstacle_direction) > THRESHOLD_FOLLOW_ANGLE)
 		{
 			direction_error = 90 * map_info.obstacle_direction/fabsf(map_info.obstacle_direction) - map_info.obstacle_direction;
-			lateral_distance = 4 * map_info.obstacle_direction/fabsf(map_info.obstacle_direction);
+			lateral_distance = WALL_FOLLOW_DISTANCE * map_info.obstacle_direction/fabsf(map_info.obstacle_direction);
+		}
+		else if(state == AVOIDING) //Dog_bot gets angry if there is too many obstacles (it was already was avoiding an obstacle)
+		{
+			state = UPSET;
+			state_change = TRUE;
 		}
 		else
 		{
 			direction_error = 180 - map_info.obstacle_direction;
 			direction_error>180?direction_error-=360:0;
+
 			last_direction_time = ST2MS(chVTGetSystemTime());
-			avoidance_manoeuver_flag = 1;
-			lateral_distance = 0;
+			
+			state = AVOIDING;
 		}
 		new_direction_flag = 1;
 	}
-
 }
 
 void follow_trajectory(void)
 {
-	//goTo(direction_error, new_direction_flag, lateral_distance);
+	goTo(direction_error, new_direction_flag, lateral_distance);
 
 	if(new_direction_flag)
 	{
@@ -157,7 +204,6 @@ void led_showDirection(void)
 
 	clear_leds();
 	set_front_led(0);
-	set_body_led(0);
 
 	switch (led_number)
 	{
@@ -192,19 +238,39 @@ void led_showDirection(void)
 	}
 }
 
-void led_showMood(void)
+void led_showUpset(bool new_call)
 {
+	static systime_t last_led_toggle = 0;
+	systime_t now = ST2MS(chVTGetSystemTime());
+
+	if(new_call)
+	{
+		toggle_rgb_led(LED2, RED_LED, 255);
+		toggle_rgb_led(LED4, RED_LED, 255);
+		set_led(LED3, 2); //2 for toggle
+	}
+
+	if((now - last_led_toggle) > UPSET_LED_TOGGLE_TIME)
+	{
+		set_led(LED5, 1);
+		set_led(LED1, 1);
+		toggle_rgb_led(LED2, RED_LED, 255);
+		toggle_rgb_led(LED4, RED_LED, 255);
+		toggle_rgb_led(LED6, RED_LED, 255);
+		toggle_rgb_led(LED8, RED_LED, 255);
+		set_led(LED3, 2);
+		set_led(LED7, 2);
+
+		last_led_toggle = now;
+	}
 
 }
 
 void led_standBy(void)
 {
-	clear_leds();
-	set_front_led(0);
-
 	int duty_cycle = 0;
 
-	duty_cycle = (sinf((float)ST2MS(chVTGetSystemTime())/200.) + 1) * 4900 + 200;
+	duty_cycle = (sinf(M_PI * (float)ST2MS(chVTGetSystemTime())/BODY_LED_PULSE_SPEED_FACTOR) + 1) * (DUTY_CYCLE_MAX - MINIMUM_DUTY_CYCLE)/2 + MINIMUM_DUTY_CYCLE;
 
 	pwmEnableChannel(&PWMD5, 0, PWM_PERCENTAGE_TO_WIDTH(&PWMD5, duty_cycle));
 }
--- Dog_bot/dog_mode.h
+++ Dog_bot/dog_mode.h
@@ -1,6 +1,10 @@
 #ifndef DOG_MODE_H
 #define DOG_MODE_H
 
+#define FOLLOWING_TIMEOUT	7 //[s]
+
+enum STATES{STAND_BY, FOLLOWING, AVOIDING, UPSET};
+
 void dog_mode_setUp(void);
 
 void playTheDog(void);
@@ -9,7 +13,7 @@ void compute_trajectory(void);
 void follow_trajectory(void);
 
 void led_showDirection(void);
-void led_showMood(void);
+void led_showUpset(bool new_call);
 void led_standBy(void);
 
 #endif /* DOG_MODE_H */
