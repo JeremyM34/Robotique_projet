301b8394db61655be8e305dcd4ff9360fecbf88a
jeremy <jeremy.marciacq@epfl.ch>
2021-05-10 13:18:24 +0200

    motor_controller optimized

--- Dog_bot/motor_controller.c
+++ Dog_bot/motor_controller.c
@@ -11,25 +11,39 @@
 #define DEDTORAD(n)	n * M_PI / 180
 #define USTOS(n)	n/1000000.
 
-#define NSTEP_ONE_TURN      1000 // number of step for 1 turn of the motor
-#define WHEEL_PERIMETER     12.8 // [cm]
-#define	WHEEL_TO_WHEEL_DIST	5.31	// [cm]
+#define NSTEP_ONE_TURN      1000 		// number of step for 1 turn of the motor
+#define WHEEL_PERIMETER     12.8 		// [cm]
+#define	WHEEL_TO_WHEEL_DIST	5.31		// [cm]
 
-#define P	2	//Proportional term (error to e_puck rotation speed)
-#define I	0.2	//Integral term (regulate perpendicular drift from initial straight line to direction)
-#define Kv	10	//Proportional term (regulate front speed depending on direction error)
+#define Kp	2		//Proportional term (error to e_puck rotation speed)
+#define Ki	0.2		//Integral term (regulate perpendicular drift from initial straight line to direction)
+#define Kspeed	10	//Proportional term (regulate front speed depending on direction error)
 
-static systime_t dt = 0; //[us] time elapsed between each loop
+#define MAX_REFERENCE_ANGLE M_PI/4 		// [rad] Maximal reference to follow to compensate perpendicular drift
 
-static float initial_error = 0;
-static float alpha_error = 0; //direction error in [rad]
-static float last_alpha_error = 0; //last direction error in [rad]
-static float perpendicular_error = 0; //perpendicular position error in [cm]
-static float front_speed = 0; //speed toward the front of the e_puck in [cm/s]
-static float w_z = 0; //rotational speed of the e_puck
+//static systime_t dt = 0; 				// [us] time elapsed between each loop - Not used anymore.
 
-static float last_dist_left = 0;
-static float last_dist_right = 0;
+static float alpha_error = 0; 			// [rad] direction error
+static float last_alpha_error = 0; 		// [rad]
+static float initial_alpha_error = 0; 	// [rad]
+static float perpendicular_error = 0; 	// [cm] perpendicular position error
+static float front_speed = 0; 			// [cm/s] speed toward the front of the e_puck
+static float w_z = 0; 					// [rad/s] rotational speed of the e_puck
+
+static float last_dist_left = 0; 		// [cm] distance travelled by a wheel during the last loop
+static float last_dist_right = 0; 		// [cm]
+
+////// PRIVATE FUNCTIONS ///////
+static void compute_controls(void);
+static void actuate(void);
+static float front_speed_calc(void);
+static float rotation_calc(void);
+//static void compute_dt(void); // Not used anymore.
+////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////// PUBLIC FUNCTIONS ///////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*
 *	Wrapper to call the inits during startup.
@@ -49,18 +63,19 @@ void motor_controller_setUp(void)
 *	Params:
 *		- direction : angle [deg]  describing where to go (useful only when new direction to go is computed)
 *		- flag_new : 1 if the direction is new, 0 if not.
+		- lateral_distance : distance [cm], where the e-puck has to go laterally
 */
 void goTo(float direction, bool flag_new, int lateral_distance)
 {
-	compute_dt();
+	//compute_dt(); - Not used anymore.
 
 	if(flag_new) //new direction to follow
 	{
-		initial_error = DEDTORAD(direction);
+		initial_alpha_error = DEDTORAD(direction);
 		alpha_error = DEDTORAD(direction);
 		perpendicular_error = lateral_distance;
 
-		left_motor_set_pos(0); //reset motor step counter for orientation determination
+		left_motor_set_pos(0); //reset motor step counter for orientation and position determination
 		right_motor_set_pos(0);
 
 		last_dist_left = 0;
@@ -68,9 +83,9 @@ void goTo(float direction, bool flag_new, int lateral_distance)
 	}
 	else //no new direction to go
 	{
-		alpha_error = initial_error - rotation_calc();
+		alpha_error = initial_alpha_error - rotation_calc();
 
-		perpendicular_error += sinf((alpha_error + last_alpha_error)/2) * front_speed_calc(); // * USTOS((float)dt);
+		perpendicular_error += sinf((alpha_error + last_alpha_error)/2) * front_speed_calc();
 	}
 
 	compute_controls();
@@ -86,26 +101,32 @@ void stop(void)
 	left_motor_set_speed(0);
 }
 
+float get_actual_error(void)
+{
+	return alpha_error*180/M_PI;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////// PRIVATE FUNCTIONS //////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /*
 *	Computes how much the e_puck should rotate on its vertical axis and the desired front speed.
 *	We use reference_angle to compensate for perpendicular motion to the line going from e_puck to its destination.
 */
 void compute_controls(void)
 {
-	float reference_angle = -I * perpendicular_error;	//[rad], new reference to follow
+	float reference_angle = -Ki * perpendicular_error;	//[rad], new reference to follow
 
-	if(reference_angle > M_PI/4)
-	{
-		reference_angle = M_PI/4;
-	}
-	else if(reference_angle < - M_PI/4)
-	{
-		reference_angle = - M_PI/4;
-	}
+	if(fabsf(reference_angle) > MAX_REFERENCE_ANGLE) // Limit to the reference
+		reference_angle = MAX_REFERENCE_ANGLE * reference_angle / fabsf(reference_angle);
 
-	w_z = -P * (reference_angle - alpha_error); //[rad/s], proportional compensation
+	w_z = -Kp * (reference_angle - alpha_error); //[rad/s], proportional compensation
 
-	front_speed = Kv * cosf(alpha_error); //[cm/s], goes faster as it points more toward the target
+	front_speed = Kspeed * cosf(alpha_error); //[cm/s], goes faster as it points more toward the target
 }
 
 /*
@@ -114,16 +135,17 @@ void compute_controls(void)
 */
 void actuate(void)
 {
+	/////// Wheel speed for e-puck rotation ///////
 	float rotation_speed = w_z * WHEEL_TO_WHEEL_DIST/2 * NSTEP_ONE_TURN / WHEEL_PERIMETER; //[step/s], initial rotational speed for each wheel
 
-	if(fabsf(rotation_speed) > MOTOR_SPEED_LIMIT)
-	{
+	if(fabsf(rotation_speed) > MOTOR_SPEED_LIMIT) // Limit to the wheel rotation speed
 		rotation_speed = MOTOR_SPEED_LIMIT * rotation_speed / fabsf(rotation_speed);
-	}
 
+	/////// Wheel speed for e-puck rotation + forward motion ///////
 	float speed_left_wheel = front_speed * NSTEP_ONE_TURN / WHEEL_PERIMETER + rotation_speed; //[step/s]
 	float speed_right_wheel = front_speed * NSTEP_ONE_TURN / WHEEL_PERIMETER - rotation_speed; //[step/s]
 
+	// Limit and recomputation if needed to avoid saturation and cancellation of the rotation speed of the e-puck
 	if (fabsf(speed_left_wheel) > MOTOR_SPEED_LIMIT || fabsf(speed_left_wheel) > MOTOR_SPEED_LIMIT) {
 		if(front_speed >= 0)
 		{
@@ -166,16 +188,16 @@ void actuate(void)
 */
 float front_speed_calc(void)
 {
-	float dist_left = (float)left_motor_get_pos() * WHEEL_PERIMETER/NSTEP_ONE_TURN;	//[cm], compute distance travelled by a wheel
-	float dist_right = (float)right_motor_get_pos() * WHEEL_PERIMETER/NSTEP_ONE_TURN;//[cm]
+	float dist_left = (float)left_motor_get_pos() * WHEEL_PERIMETER/NSTEP_ONE_TURN;		//[cm], compute distance travelled by a wheel since the begining
+	float dist_right = (float)right_motor_get_pos() * WHEEL_PERIMETER/NSTEP_ONE_TURN;	//[cm]
 
-	float short_dist_left = dist_left - last_dist_left;
-	float short_dist_right = dist_right - last_dist_right;
+	float short_dist_left = dist_left - last_dist_left; 	// [cm], distance travelled by a wheel in a loop
+	float short_dist_right = dist_right - last_dist_right;	// [cm]
 
 	last_dist_right = dist_right;
 	last_dist_left = dist_left;
 
-	return (short_dist_left+short_dist_right)/2;
+	return (short_dist_left+short_dist_right)/2; // tiny distance travelled in [cm]
 }
 
 /*
@@ -183,10 +205,8 @@ float front_speed_calc(void)
 */
 float rotation_calc(void)
 {
-	float dist_left = (float)left_motor_get_pos() * WHEEL_PERIMETER/NSTEP_ONE_TURN;	//[cm], compute distance travelled by a wheel
-	float dist_right = (float)right_motor_get_pos() * WHEEL_PERIMETER/NSTEP_ONE_TURN;//[cm]
-
-	//chprintf((BaseSequentialStream *) &SD3, "left_motor_get_pos = %3f; right_motor_get_pos = %3f \n", (float)left_motor_get_pos(), (float)right_motor_get_pos());
+	float dist_left = (float)left_motor_get_pos() * WHEEL_PERIMETER/NSTEP_ONE_TURN;		//[cm], compute distance travelled by a wheel since the begining
+	float dist_right = (float)right_motor_get_pos() * WHEEL_PERIMETER/NSTEP_ONE_TURN;	//[cm]
 
 	return (dist_left-dist_right)/(WHEEL_TO_WHEEL_DIST);	//angle travelled in [rad]
 }
@@ -194,6 +214,7 @@ float rotation_calc(void)
 /*
 *	Used to compute the elapsed time between each loop.
 */
+/* Not used anymore
 void compute_dt(void)
 {	
 	static systime_t last_time = 0; //last measured time init in system ticks
@@ -201,12 +222,6 @@ void compute_dt(void)
 	systime_t time = chVTGetSystemTime(); //time in system ticks
 	dt = ST2US(time - last_time);
 
-	//chprintf((BaseSequentialStream *) &SD3, "dt = %d ; ", chVTGetSystemTime());
-
 	last_time = time;
 }
-
-float get_actual_error(void)
-{
-	return alpha_error*180/M_PI;
-}
+*/
--- Dog_bot/motor_controller.h
+++ Dog_bot/motor_controller.h
@@ -1,17 +1,24 @@
 #ifndef MOTOR_CONTROLLER_H
 #define MOTOR_CONTROLLER_H
 
+/*
+*	Function to be called for motor initialization.
+*/
 void motor_controller_setUp(void);
 
+/*
+*   Motor control loop : make the e-puck move toward a direction without having to call for a new direction at each loop.
+*	Params:
+*		- direction : angle [deg]  describing where to go (useful only when new direction to go is computed)
+*		- flag_new : 1 if the direction is new, 0 if not.
+		- lateral_distance : distance [cm], where the e-puck has to go laterally
+*/
 void goTo(float direction, bool flag_new, int lateral_distance);
+
+/*
+*	Function to stop the motors.
+*/
 void stop(void);
-
-void compute_controls(void);
-void actuate(void);
-
-float front_speed_calc(void);
-float rotation_calc(void);
-void compute_dt(void);
 
 float get_actual_error(void);
 
